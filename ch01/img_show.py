815f7b1c2f501e8109b7c3a8a3709ee2c03161e8
hyrodium <hyrodium@gmail.com>
2020-01-15 18:12:58 +0900

    finish ch01

--- ch01/img_show.py
+++ ch01/img_show.py
@@ -1,8 +1,10 @@
 # coding: utf-8
+import os
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
-img = imread('../dataset/lena.png') # 画像の読み込み
+
+img = imread(os.getcwd()+'/dataset/lena.png')  # 画像の読み込み
 plt.imshow(img)
 
 plt.show()
