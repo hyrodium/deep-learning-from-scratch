cc2befb1c5c6b5a5d52f9464786ce5401597d8a0
hyrodium <hyrodium@gmail.com>
2020-01-16 18:21:19 +0900

    finish ch07

--- ch07/apply_filter.py
+++ ch07/apply_filter.py
@@ -1,37 +1,39 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
-import numpy as np
+import os
 import matplotlib.pyplot as plt
 from simple_convnet import SimpleConvNet
 from matplotlib.image import imread
-from common.layers import Convolution
+from DLFS.common.layers import Convolution
+
 
 def filter_show(filters, nx=4, show_num=16):
     """
-    c.f. https://gist.github.com/aidiary/07d530d5e08011832b12#file-draw_weight-py
+    c.f.
+    https://gist.github.com/aidiary/07d530d5e08011832b12#file-draw_weight-py
     """
     FN, C, FH, FW = filters.shape
-    ny = int(np.ceil(show_num / nx))
+    # ny = int(np.ceil(show_num / nx))
 
     fig = plt.figure()
-    fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+    fig.subplots_adjust(left=0, right=1, bottom=0,
+                        top=1, hspace=0.05, wspace=0.05)
 
     for i in range(show_num):
         ax = fig.add_subplot(4, 4, i+1, xticks=[], yticks=[])
         ax.imshow(filters[i, 0], cmap=plt.cm.gray_r, interpolation='nearest')
 
 
-network = SimpleConvNet(input_dim=(1,28,28), 
-                        conv_param = {'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
+network = SimpleConvNet(input_dim=(1, 28, 28),
+                        conv_param={'filter_num': 30,
+                                    'filter_size': 5, 'pad': 0, 'stride': 1},
                         hidden_size=100, output_size=10, weight_init_std=0.01)
 
 # 学習後の重み
-network.load_params("params.pkl")
+network.load_params(os.getcwd()+"/ch07/params.pkl")
 
 filter_show(network.params['W1'], 16)
 
-img = imread('../dataset/lena_gray.png')
+img = imread(os.getcwd()+"/dataset/lena_gray.png")
 img = img.reshape(1, 1, *img.shape)
 
 fig = plt.figure()
@@ -43,11 +45,11 @@ for i in range(16):
     b = 0  # network.params['b1'][i]
 
     w = w.reshape(1, *w.shape)
-    #b = b.reshape(1, *b.shape)
-    conv_layer = Convolution(w, b) 
+    # b = b.reshape(1, *b.shape)
+    conv_layer = Convolution(w, b)
     out = conv_layer.forward(img)
     out = out.reshape(out.shape[2], out.shape[3])
-    
+
     ax = fig.add_subplot(4, 4, i+1, xticks=[], yticks=[])
     ax.imshow(out, cmap=plt.cm.gray_r, interpolation='nearest')
 
--- ch07/simple_convnet.py
+++ ch07/simple_convnet.py
@@ -1,18 +1,17 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 import pickle
 import numpy as np
 from collections import OrderedDict
-from common.layers import *
-from common.gradient import numerical_gradient
+from DLFS.common.layers \
+    import Convolution, Relu, Pooling, Affine, SoftmaxWithLoss
+from DLFS.common.gradient import numerical_gradient
 
 
 class SimpleConvNet:
     """単純なConvNet
 
     conv - relu - pool - affine - relu - affine - softmax
-    
+
     Parameters
     ----------
     input_size : 入力サイズ（MNISTの場合は784）
@@ -23,33 +22,38 @@ class SimpleConvNet:
         'relu'または'he'を指定した場合は「Heの初期値」を設定
         'sigmoid'または'xavier'を指定した場合は「Xavierの初期値」を設定
     """
-    def __init__(self, input_dim=(1, 28, 28), 
-                 conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
+
+    def __init__(self, input_dim=(1, 28, 28),
+                 conv_param={'filter_num': 30,
+                             'filter_size': 5, 'pad': 0, 'stride': 1},
                  hidden_size=100, output_size=10, weight_init_std=0.01):
         filter_num = conv_param['filter_num']
         filter_size = conv_param['filter_size']
         filter_pad = conv_param['pad']
         filter_stride = conv_param['stride']
         input_size = input_dim[1]
-        conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
-        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+        conv_output_size = (input_size - filter_size + 2 *
+                            filter_pad) / filter_stride + 1
+        pool_output_size = int(
+            filter_num * (conv_output_size/2) * (conv_output_size/2))
 
         # 重みの初期化
         self.params = {}
         self.params['W1'] = weight_init_std * \
-                            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
         self.params['b1'] = np.zeros(filter_num)
         self.params['W2'] = weight_init_std * \
-                            np.random.randn(pool_output_size, hidden_size)
+            np.random.randn(pool_output_size, hidden_size)
         self.params['b2'] = np.zeros(hidden_size)
         self.params['W3'] = weight_init_std * \
-                            np.random.randn(hidden_size, output_size)
+            np.random.randn(hidden_size, output_size)
         self.params['b3'] = np.zeros(output_size)
 
         # レイヤの生成
         self.layers = OrderedDict()
-        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
-                                           conv_param['stride'], conv_param['pad'])
+        self.layers['Conv1'] = \
+            Convolution(self.params['W1'], self.params['b1'],
+                        conv_param['stride'], conv_param['pad'])
         self.layers['Relu1'] = Relu()
         self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
         self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
@@ -72,17 +76,18 @@ class SimpleConvNet:
         return self.last_layer.forward(y, t)
 
     def accuracy(self, x, t, batch_size=100):
-        if t.ndim != 1 : t = np.argmax(t, axis=1)
-        
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
+
         acc = 0.0
-        
+
         for i in range(int(x.shape[0] / batch_size)):
             tx = x[i*batch_size:(i+1)*batch_size]
             tt = t[i*batch_size:(i+1)*batch_size]
             y = self.predict(tx)
             y = np.argmax(y, axis=1)
-            acc += np.sum(y == tt) 
-        
+            acc += np.sum(y == tt)
+
         return acc / x.shape[0]
 
     def numerical_gradient(self, x, t):
@@ -99,12 +104,14 @@ class SimpleConvNet:
             grads['W1']、grads['W2']、...は各層の重み
             grads['b1']、grads['b2']、...は各層のバイアス
         """
-        loss_w = lambda w: self.loss(x, t)
+        def loss_w(w): return self.loss(x, t)
 
         grads = {}
         for idx in (1, 2, 3):
-            grads['W' + str(idx)] = numerical_gradient(loss_w, self.params['W' + str(idx)])
-            grads['b' + str(idx)] = numerical_gradient(loss_w, self.params['b' + str(idx)])
+            grads['W' + str(idx)] = \
+                numerical_gradient(loss_w, self.params['W' + str(idx)])
+            grads['b' + str(idx)] = \
+                numerical_gradient(loss_w, self.params['b' + str(idx)])
 
         return grads
 
@@ -136,12 +143,15 @@ class SimpleConvNet:
 
         # 設定
         grads = {}
-        grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
-        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
-        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+        grads['W1'], grads['b1'] = \
+            self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = \
+            self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = \
+            self.layers['Affine2'].dW, self.layers['Affine2'].db
 
         return grads
-        
+
     def save_params(self, file_name="params.pkl"):
         params = {}
         for key, val in self.params.items():
