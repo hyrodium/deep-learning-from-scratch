6be1440777891b31688824c00fcd1cbc1138fd86
hyrodium <hyrodium@gmail.com>
2020-01-16 14:49:51 +0900

    finish ch04

--- ch04/gradient_2d.py
+++ ch04/gradient_2d.py
@@ -2,24 +2,24 @@
 # cf.http://d.hatena.ne.jp/white_wheels/20100327/p3
 import numpy as np
 import matplotlib.pylab as plt
-from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 
 
 def _numerical_gradient_no_batch(f, x):
     h = 1e-4  # 0.0001
     grad = np.zeros_like(x)
-    
+
     for idx in range(x.size):
         tmp_val = x[idx]
         x[idx] = float(tmp_val) + h
         fxh1 = f(x)  # f(x+h)
-        
-        x[idx] = tmp_val - h 
+
+        x[idx] = tmp_val - h
         fxh2 = f(x)  # f(x-h)
         grad[idx] = (fxh1 - fxh2) / (2*h)
-        
+
         x[idx] = tmp_val  # 値を元に戻す
-        
+
     return grad
 
 
@@ -28,10 +28,10 @@ def numerical_gradient(f, X):
         return _numerical_gradient_no_batch(f, X)
     else:
         grad = np.zeros_like(X)
-        
+
         for idx, x in enumerate(X):
             grad[idx] = _numerical_gradient_no_batch(f, x)
-        
+
         return grad
 
 
@@ -53,14 +53,14 @@ if __name__ == '__main__':
     x0 = np.arange(-2, 2.5, 0.25)
     x1 = np.arange(-2, 2.5, 0.25)
     X, Y = np.meshgrid(x0, x1)
-    
+
     X = X.flatten()
     Y = Y.flatten()
 
     grad = numerical_gradient(function_2, np.array([X, Y]).T).T
 
     plt.figure()
-    plt.quiver(X, Y, -grad[0], -grad[1],  angles="xy",color="#666666")
+    plt.quiver(X, Y, -grad[0], -grad[1],  angles="xy", color="#666666")
     plt.xlim([-2, 2])
     plt.ylim([-2, 2])
     plt.xlabel('x0')
--- ch04/gradient_simplenet.py
+++ ch04/gradient_simplenet.py
@@ -1,14 +1,15 @@
 # coding: utf-8
-import sys, os
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+from DLFS.common.gradient import numerical_gradient
+from DLFS.common.functions import softmax, cross_entropy_error
 import numpy as np
-from common.functions import softmax, cross_entropy_error
-from common.gradient import numerical_gradient
+import sys
+import os
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
 
 
 class simpleNet:
     def __init__(self):
-        self.W = np.random.randn(2,3)
+        self.W = np.random.randn(2, 3)
 
     def predict(self, x):
         return np.dot(x, self.W)
@@ -20,12 +21,16 @@ class simpleNet:
 
         return loss
 
+
 x = np.array([0.6, 0.9])
 t = np.array([0, 0, 1])
 
 net = simpleNet()
 
-f = lambda w: net.loss(x, t)
+
+def f(w): return net.loss(x, t)
+
+
 dW = numerical_gradient(f, net.W)
 
 print(dW)
